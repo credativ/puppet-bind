aca9f2c524d4ac76b23148f769c9df54e66999c4
Bastian Blank <bastian.blank@credativ.de>
2015-03-27 15:16:46 +0100

    Add tests for bind config

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -5,14 +5,34 @@ describe 'bind' do
   context "With default facts" do
     it {
       should contain_class('bind')
+      should_not contain_file('/etc/bind/named.conf.options')
+    }
+  end
+
+  context "With config" do
+    let (:params) { { :manage_config => true, :forwarders => [] } }
+
+    it {
+      should contain_file('/etc/bind/named.conf.options')
+        .without_content(%r{^\s*forward\s+})
     }
   end
 
   context "With valid forward" do
-    let (:params) { { :forward => 'only' } }
+    let (:params) { { :manage_config => true, :forwarders => [], :forward => 'only' } }
 
     it {
-      should contain_class('bind')
+      should contain_file('/etc/bind/named.conf.options')
+        .with_content(%r{^\s*forward\s+only;})
+    }
+  end
+
+  context "With empty forward" do
+    let (:params) { { :manage_config => true, :forwarders => [], :forward => '' } }
+
+    it {
+      should contain_file('/etc/bind/named.conf.options')
+        .without_content(%r{^\s*forward\s+})
     }
   end
 
